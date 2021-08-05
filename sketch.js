08846b03ea21af8fdffd77a092aa3b7a73dd70ed
julio507 <julio.rosa@universo.univates.br>
2021-08-05 00:05:14 -0300

    first commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(600, 400);
+}
+
+function draw() {
+    background(220);
+
+    quad(10, 10, 590, 10, 590, 390, 10, 390);
+
+    line(500, 390, 500, 75);
+    line(500, 75, 400, 75);
+    line(400, 75,)
+}
