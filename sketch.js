c8f77d9d8e5b1ceb54a59215070d28295a608317
julio507 <julio.rosa@universo.univates.br>
2021-08-05 00:23:51 -0300

    criacao de parte da pista

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
 function setup() {
     createCanvas(600, 400);
-}
 
-function draw() {
     background(220);
 
     quad(10, 10, 590, 10, 590, 390, 10, 390);
 
     line(500, 390, 500, 75);
     line(500, 75, 400, 75);
-    line(400, 75,)
+    line(400, 75, 400, 200 );
+    line(400, 200, 425, 200 );
+    line( 425, 200, 425, 325 );
+}
+
+function draw() {
+
 }
